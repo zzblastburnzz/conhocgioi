dbbc82829ff9b8d8da89cc4cc18e6cc16fa9318d
Ant <thanhpc.luxuryhome@gmail.com>
2025-03-26 12:50:57 +0700

    Create alphabetData.js

---
+++ alphabetData.js
@@ -0,0 +1,17 @@
+
+const alphabetData = {
+  vi: [
+    { letter: "A", word: "Áo", image: "assets/words/ao.png", audio: "audio/vi/a.mp3" },
+    { letter: "B", word: "Bò", image: "assets/words/bo.png", audio: "audio/vi/b.mp3" },
+    { letter: "C", word: "Cá", image: "assets/words/ca.png", audio: "audio/vi/c.mp3" },
+    { letter: "D", word: "Đèn", image: "assets/words/den.png", audio: "audio/vi/d.mp3" },
+    { letter: "E", word: "Ếch", image: "assets/words/ech.png", audio: "audio/vi/e.mp3" }
+  ],
+  en: [
+    { letter: "A", word: "Apple", image: "assets/words/apple.png", audio: "audio/en/a.mp3" },
+    { letter: "B", word: "Ball", image: "assets/words/ball.png", audio: "audio/en/b.mp3" },
+    { letter: "C", word: "Cat", image: "assets/words/cat.png", audio: "audio/en/c.mp3" },
+    { letter: "D", word: "Dog", image: "assets/words/dog.png", audio: "audio/en/d.mp3" },
+    { letter: "E", word: "Egg", image: "assets/words/egg.png", audio: "audio/en/e.mp3" }
+  ]
+};
